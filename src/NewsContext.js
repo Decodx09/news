49871eeeae361c726bba5d4da214b972576aa83f
Shivansh Sukhija <sukhija.shivansh@gmail.com>
2022-05-10 16:54:37 +0530

    something

---
+++ src/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "0d574d776bd14fbd8fc11198d1a40191";
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://newsapi.org/v2/everything?q=rich&from=2022-05-10&sortBy=publishedAt&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
